50f93c110bb9bd3eb4018ba58623a3a9ab5af768
Kokonattsu <88546717+00Kokonattsu@users.noreply.github.com>
2021-08-09 11:25:08 +0700

    Add files via upload

---
+++ Progdas1.java
@@ -0,0 +1,31 @@
+public class Progdas1 {
+    public static void main(String[] args) throws Exception {
+        // psvm (public static void main ((netbeans))
+        // main (public static void main ((vscode))
+        // digunakan untuk menunjukkan apa yang didefinisikan
+
+        // String digunakan untuk memasukkan kata dan angka (pakai "")
+        String nama = "Fauzan Farhan";
+        String kelas = "X RPL 8";
+        // int digunakan untuk memasukkan angka (tanpa "")
+        int absen = 19;
+        int umur = 14;
+        int tinggi = 165;
+        // Char digunakan untuk memasukkan 1 huruf (pakai '')
+        char jeniskelamin = 'L';
+        // double digunakan untuk memasukkan angka yang lebih banyak dari int
+        double beratbadan = 59;
+        // Boolean dipakai untuk menentukan sesuatu
+        boolean hadir = true;
+        // System.out.print digunakan untuk mengeluarkan hasil definisi
+        System.out.println("Nama            : " + nama);
+        System.out.println("Kelas           : " + kelas);
+        System.out.println("Absen           : " + absen);
+        System.out.println("Jenis kelamin   : " + jeniskelamin);
+        System.out.println("Umur            : " + umur);
+        System.out.println("Tinggi          : " + tinggi + "cm");
+        System.out.println("Berat badan     : " + beratbadan + "kg");
+        System.out.println("Hadir           : " + hadir);
+
+    }
+}
